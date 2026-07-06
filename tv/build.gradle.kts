9e11c0cfa059232b6c2c8557a805e43c43daf913
Lau1907 <130590443+Lau1907@users.noreply.github.com>
2026-07-06 17:26:02 -0600

    chore: add Android TV module with Leanback setup, MainActivity and Theme

---
+++ tv/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "mx.utng.smarthealthmonitor.tv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "mx.utng.smarthealthmonitor.tv"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation("androidx.leanback:leanback:1.2.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(project(":app"))
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+}
